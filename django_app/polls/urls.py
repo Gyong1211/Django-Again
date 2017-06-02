dc70a74c92e5db981b4737e75214b2d14907b882
Gihong Kim <gyong1211@gmail.com>
2017-06-02 11:37:04 +0900

    modified index view(use template), add template(polls/index.html)

--- django_app/polls/urls.py
+++ django_app/polls/urls.py
@@ -3,7 +3,9 @@ from django.conf.urls import url
 from polls import views
 
 # from . import views와 같음
-
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote')
 ]
