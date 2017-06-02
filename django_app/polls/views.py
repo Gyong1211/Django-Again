cb5d2cd321fac870ae0860fe98faf2b1e88622a9
Gihong Kim <gyong1211@gmail.com>
2017-06-02 14:48:22 +0900

    add form to detail.html, vote POST function

--- django_app/polls/views.py
+++ django_app/polls/views.py
@@ -1,7 +1,8 @@
+from django.contrib import messages
 from django.http import HttpResponse, Http404
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
 
-from polls.models import Question
+from polls.models import Question, Choice
 
 
 def index(request):
@@ -15,7 +16,7 @@ def index(request):
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     choices = question.choice_set.all()
-    #choice = Choice.object.all(question = question)
+    # choice = Choice.object.all(question = question)
     context = {
         'question': question,
         'choices': choices,
@@ -30,4 +31,21 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    if request.method == 'POST':
+        data = request.POST
+        try:
+            choice_id = data['choice']
+            choice = Choice.objects.get(id=choice_id)
+            choice.votes +=1
+            choice.save()
+            # return HttpResponse("You're voting {} on question {}.".format(select, question_id, ))
+            return redirect('polls:results',question_id)
+        except (KeyError,Choice.DoesNotExist):
+            messages.add_message(
+                request,
+                messages.ERROR,
+                "You didn't select a choice"
+            )
+            return redirect('polls:detail',question_id)
+    elif request.method == 'GET':
+        return HttpResponse("You're not voting on question %s." % question_id)
